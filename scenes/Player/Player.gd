d432a686e105215351b9e1fccd5a1ad14995554b
Arrol Cardoz <cardozarrol@gmail.com>
2025-07-30 13:10:24 -0600

    Item resources and pizza

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
 @export var MOVE_SPEED:float=250
+@onready var ray_cast: RayCast2D = $RayCast2D
+
+var _previousDir:Vector2=Vector2.RIGHT
+
+const RAYCASE_LENGTH:float=50
 
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var moveVec:Vector2=process_input()
+
+	if moveVec==Vector2.ZERO:
+		ray_cast.target_position=_previousDir*RAYCASE_LENGTH
+	else:
+		ray_cast.target_position=moveVec*RAYCASE_LENGTH
+		_previousDir=moveVec
+
+
 	velocity=moveVec*MOVE_SPEED
 	move_and_slide()
 
