d27dce7cc9eccfaa1104257f06a57b83141d2a36
Arrol Cardoz <cardozarrol@gmail.com>
2025-08-02 19:52:21 -0600

    wheatBag and trashCan

---
+++ TrashCan.gd
@@ -0,0 +1,14 @@
+extends ItemHolder
+class_name trashCan
+
+@onready var sound: AudioStreamPlayer2D = $sound
+
+const STATION_NAME:String="trashCan"
+
+
+func remove_item()->void:
+	pass
+
+func place_item(item)->void:
+	for marker in markers.get_children():
+		if _item==null:continue
---
+++ scenes/ItemHolder/WheatBag.gd
@@ -0,0 +1,18 @@
+extends ItemHolder
+class_name wheatBag
+
+@onready var sound: AudioStreamPlayer2D = $sound
+const DOUGH = preload("res://resources/Items/dough.tres")
+const STATION_NAME:String="trashCan"
+
+func _ready() -> void:
+	super()
+	_isFull=true
+
+func getItem()->Item:return DOUGH
+
+func remove_item()->void:
+	pass
+
+func place_item(item)->void:
+	pass
